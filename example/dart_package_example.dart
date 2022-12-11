9f276ca3c8c821dd9fb20641efb7082e966ba832
Tharinda Nimnajith Rajapaksha <tharindarajapakshe@gmail.com>
2022-12-11 14:53:56 +0530

    init

---
+++ example/dart_package_example.dart
@@ -0,0 +1,6 @@
+import 'package:dart_package/dart_package.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/dart_package.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dart_package;
+
+export 'src/dart_package_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
